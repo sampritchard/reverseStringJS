2054c5d7a7581b291f1da3bb065a70d4bbc2c4af
Sam Pritchard <sampritchard@msn.com>
2017-09-21 14:41:01 +0100

    Created solution where a new string is created

--- src/String.js
+++ src/String.js
@@ -1,5 +1,11 @@
 function reverse(string) {
   if (string.length <= 1 ) {
     return string;
+  } else {
+    var newString = "";
+    for (var i = string.length -1; i >=0; i--) {
+      newString += string[i];
+    }
+    return newString;
   }
 }
